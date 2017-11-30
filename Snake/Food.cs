421de4b97f0438da31287b1eba71b67d46c4aa30
siamcio <smoogus@gmail.com>
2017-11-30 17:08:57 +0100

    +food

---
+++ Snake/Food.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Food
+    {
+        int posx;
+        int posy;
+
+        public int Posx { get => posx; set => posx = value; }
+        public int Posy { get => posy; set => posy = value; }
+    }
+}
---
+++ Snake/FoodUI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Snake
+{
+    class FoodUI
+    {
+        Food food;
+        Rectangle Rect = new Rectangle();
+
+        public FoodUI(Food food, Canvas canvas)
+        {
+            this.Food = food;
+            canvas.Children.Add(Rect);
+        }
+        public void Draw(Canvas canvas)
+        {
+            Rect.Width = canvas.Width * 0.1;
+            Rect.Height = canvas.Width * 0.1;
+            Rect.Fill = Brushes.Red;
+            Canvas.SetLeft(Rect, (double)((food.Posx * 0.1)) * canvas.Width);
+            Canvas.SetTop(Rect, (food.Posy * 0.1) * canvas.Height);
+
+        }
+
+        internal Food Food { get => food; set => food = value; }
+        public Rectangle Rect1 { get => Rect; set => Rect = value; }
+    }
+}
