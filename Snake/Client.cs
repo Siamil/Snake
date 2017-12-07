274ea8db04e65f89455115d1705f80b96b9de34e
siamcio <smoogus@gmail.com>
2017-12-07 18:45:57 +0100

    server and client added

---
+++ Snake/Client.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Web;
+using Newtonsoft.Json;
+namespace Snake
+{
+    class Client
+    {
+        Socket sender;
+        
+        public Client()
+        {
+            sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint endpoint = new IPEndPoint(0, 80);
+            sender.Connect(endpoint);
+
+        }
+        public void sendMessage(Game game)
+        {
+            string json = JsonConvert.SerializeObject(game.MultiDirection, Formatting.Indented);
+            
+            byte[] data = Encoding.Default.GetBytes(json);
+
+            sender.Send(data);
+        }
+        public Direction  receiveBytes()
+        {
+            byte[] data = new byte[255];
+            
+            int bytes = sender.Receive(data, 0, data.Length, 0);
+            Array.Resize(ref data, bytes);
+            string json = data.ToString();
+            Direction direction = JsonConvert.DeserializeObject<Direction>(json);
+            return direction;
+        }
+    }
+}
---
+++ Snake/Server.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using Newtonsoft.Json;
+
+
+namespace Snake
+{
+    class Server
+    {
+         Socket sender;
+         Socket client;
+
+        public Server()
+        {
+            sender = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+            sender.Bind(new IPEndPoint(0, 80));
+            sender.Listen(10);
+             client = sender.Accept();
+
+
+        }
+        public void sendBytes (Game game)
+            {
+            string json = JsonConvert.SerializeObject(game.Direction, Formatting.Indented);
+
+            byte[] data = Encoding.Default.GetBytes(json);
+
+            client.Send(data);
+        }
+        public Direction receiveBytes()
+        {
+            byte[] data = new byte[255];
+
+            int bytes = client.Receive(data, 0, data.Length, 0);
+            Array.Resize(ref data, bytes);
+            string json = data.ToString();
+            Direction direction = JsonConvert.DeserializeObject<Direction>(json);
+            return direction;
+        }
+    }
+}
